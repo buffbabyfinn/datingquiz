72135700e9a84c5b54bf606aac591907deae7324
Mollie Bootsma and Megan Fayer <mefayer@gmail.com>
2016-01-13 16:05:55 -0800

    Add answers and complexity of logic.

--- scripts.js
+++ scripts.js
@@ -8,17 +8,26 @@ $(function(event){
   $("#submit").click(function(){
     var gender = $("select#gender").val();
     var nature = $("select#nature").val();
+    var date = $("select#date").val();
 
-    if (gender === "male" && nature === "forest") {
+    if (gender === "male" && nature === "forest"  && date === "candles") {
       $('.answer2').show();
-    } else if (gender === "male" && nature === "oceans") {
+    } else if (gender === "male" && nature === "forest" && date === "nightout") {
       $('.answer4').show();
-    } else if (gender === "female" && nature === "forest") {
+    } else if (gender === "female" && nature === "forest" && date === "candles") {
       $('.answer1').show();
-    } else {
+    } else if (gender === "female" && nature === "forest" && date === "nightout") {
       $('.answer3').show();
-    }
-    
+    } else if (gender === "male" && nature === "oceans" && date === "candles") {
+        $('.answer5').show();
+      } else if (gender === "female" && nature === "oceans" && date === "nightout") {
+        $('.answer8').show();
+      } else if (gender === "female" && nature === "oceans" && date === "candles") {
+        $('.answer6').show();
+      } else {
+        $('.answer7').show();
+      }
+
     $(".test-contain").hide();
     $(".answers-contain").show();
   });
